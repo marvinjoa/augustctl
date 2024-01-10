74f330df0d0e7c3f3878f4afb0471897394fa89b
marvinjoa <marvinjoa@me.com>
2024-01-10 16:42:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,149 +1,118 @@
 'use strict';
 
-var augustctl = require('./index');
-var express = require('express');
-var morgan = require('morgan');
-var await = require('asyncawait/await');
-var async = require('asyncawait/async');
-var config = require(process.env.AUGUSTCTL_CONFIG || './config.json');
-var DEBUG = process.env.NODE_ENV !== 'production';
-var address = config.address || 'localhost';
-var port = config.port || 3000;
+const augustctl = require('./index');
+const express = require('express');
+const morgan = require('morgan');
+const config = require(process.env.AUGUSTCTL_CONFIG || './config.json');
+const DEBUG = process.env.NODE_ENV !== 'production';
+const address = config.address || 'localhost';
+const port = config.port || 3000;
 
-var app = express();
+const app = express();
 app.use(morgan(DEBUG ? 'dev' : 'combined'));
 
-var ret = {'status': -1, 'ret': '', 'msg': ''};
+const ret = { 'status': -1, 'ret': '', 'msg': '' };
 
-app.get('/api/unlock', function(req, res) {
+app.get('/api/unlock', async (req, res) => {
+  try {
+    const lock = app.get('lock');
 
-  var lock = app.get('lock');
-  
-  if (!lock) {
-    res.sendStatus(503);
-    return;
-  }
-
-  lock.connect().then(function(){
-
-		lock.status().then(function(data){
-
-			if(data == 'locked')
-			{
-
-				lock.forceUnlock().then(function(response){
-				
-					ret['msg'] = 'Command completed. Disconnected.';
-					ret['status'] = 0;
-					ret['ret'] = 'unlocked';					
-					res.json(ret);
-					
-					lock.disconnect();
-				
-				});
-
-
-			}
-			else
-			{
-				ret['status'] = 1;
-				ret['msg'] = 'Lock is already locked';
-				res.json(ret);
-				
-				lock.disconnect();
-			}
-
-
-		});
-	});
-
-});
-
-
-app.get('/api/lock', function(req, res) {
-
-  var lock = app.get('lock');
-  
-  if (!lock) {
-    res.sendStatus(503);
-    return;
-  }
-
-  lock.connect().then(function(){
-
-		lock.status().then(function(data){
-
-			if(data == 'unlocked')
-			{
-
-				lock.forceLock().then(function(response){
-				
-					ret['msg'] = 'Command completed. Disconnected.';
-					ret['status'] = 0;
-					ret['ret'] = 'locked';
-					
-					res.json(ret);
-					
-					lock.disconnect();
-				
-				});
-
-
-			}
-			else
-			{
-				ret['status'] = 1;
-				ret['msg'] = 'Lock is already locked';
-				
-				res.json(ret);
-				
-				lock.disconnect();
-			}
-
-
-		});
-	})
-
-});
-
-
-app.get('/api/status', function(req, res){
-
-   var lock = app.get('lock');
-   if(!lock) {
+    if (!lock) {
       res.sendStatus(503);
       return;
-   }
+    }
 
-   lock.connect().then(function(){
+    await lock.connect();
+    const data = await lock.status();
 
-		lock.status().then(function(data){
-
-				
-			ret['msg'] = 'Command completed. Disconnected.';
-			ret['status'] = 0;
-			ret['ret'] = data;
-		
-			res.json(ret);
-		
-			lock.disconnect();
-				
-
-		});
-	});
-
+    if (data === 'locked') {
+      const response = await lock.forceUnlock();
+      ret['msg'] = 'Command completed. Disconnected.';
+      ret['status'] = 0;
+      ret['ret'] = 'unlocked';
+      res.json(ret);
+      await lock.disconnect();
+    } else {
+      ret['status'] = 1;
+      ret['msg'] = 'Lock is already locked';
+      res.json(ret);
+      await lock.disconnect();
+    }
+  } catch (e) {
+    console.error(e.toString());
+    res.sendStatus(500);
+  }
 });
 
+app.get('/api/lock', async (req, res) => {
+  try {
+    const lock = app.get('lock');
 
-augustctl.scan(config.lockUuid).then(function(peripheral) {
-  var lock = new augustctl.Lock(
-    peripheral,
-    config.offlineKey,
-    config.offlineKeyOffset
-  );
-  app.set('lock', lock);
+    if (!lock) {
+      res.sendStatus(503);
+      return;
+    }
+
+    await lock.connect();
+    const data = await lock.status();
+
+    if (data === 'unlocked') {
+      const response = await lock.forceLock();
+      ret['msg'] = 'Command completed. Disconnected.';
+      ret['status'] = 0;
+      ret['ret'] = 'locked';
+      res.json(ret);
+      await lock.disconnect();
+    } else {
+      ret['status'] = 1;
+      ret['msg'] = 'Lock is already locked';
+      res.json(ret);
+      await lock.disconnect();
+    }
+  } catch (e) {
+    console.error(e.toString());
+    res.sendStatus(500);
+  }
 });
 
-var server = app.listen(port, address, function() {
+app.get('/api/status', async (req, res) => {
+  try {
+    const lock = app.get('lock');
+    if (!lock) {
+      res.sendStatus(503);
+      return;
+    }
+
+    await lock.connect();
+    const data = await lock.status();
+
+    ret['msg'] = 'Command completed. Disconnected.';
+    ret['status'] = 0;
+    ret['ret'] = data;
+
+    res.json(ret);
+
+    await lock.disconnect();
+  } catch (e) {
+    console.error(e.toString());
+    res.sendStatus(500);
+  }
+});
+
+augustctl.scan(config.lockUuid)
+  .then(peripheral => {
+    const lock = new augustctl.Lock(
+      peripheral,
+      config.offlineKey,
+      config.offlineKeyOffset
+    );
+    app.set('lock', lock);
+  })
+  .catch(err => {
+    console.error(err.toString());
+    process.exit(1);
+  });
+
+const server = app.listen(port, address, () => {
   console.log('Listening at %j', server.address());
 });
