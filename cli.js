6ff1a055b0f072bc071d90a526c85749789ba56f
marvinjoa <marvinjoa@me.com>
2024-01-10 16:46:07 +0100

    Update cli.js

--- cli.js
+++ cli.js
@@ -2,29 +2,36 @@
 
 'use strict';
 
-var augustctl = require('./index');
+const augustctl = require('./index');
+const util = require('util');
+const fs = require('fs').promises;
 
-var config = require(process.env.AUGUSTCTL_CONFIG || './config.json');
+async function main() {
+  const configPath = process.env.AUGUSTCTL_CONFIG || './config.json';
+  const config = JSON.parse(await fs.readFile(configPath, 'utf8'));
 
-var op = process.argv[2];
-if (typeof augustctl.Lock.prototype[op] !== 'function') {
-  throw new Error('invalid operation: ' + op);
+  const operation = process.argv[2];
+  const lockPrototype = Object.getPrototypeOf(new augustctl.Lock());
+
+  if (typeof lockPrototype[operation] !== 'function') {
+    throw new Error('Invalid operation: ' + operation);
+  }
+
+  try {
+    const peripheral = await augustctl.scan(config.lockUuid);
+    const lock = new augustctl.Lock(
+      peripheral,
+      config.offlineKey,
+      config.offlineKeyOffset
+    );
+
+    await lock.connect();
+    await lock[operation]();
+  } catch (e) {
+    console.error(e.toString());
+  } finally {
+    process.exit(0);
+  }
 }
 
-augustctl.scan(config.lockUuid).then(function(peripheral) {
-  var lock = new augustctl.Lock(
-    peripheral,
-    config.offlineKey,
-    config.offlineKeyOffset
-  );
-
-  lock.connect().then(function() {
-    return lock[op]();
-  }).catch(function(e) {
-    console.error(e.toString());
-  }).finally(function() {
-    return lock.disconnect().finally(function() {
-      process.exit(0);
-    });
-  });
-});
+main();
