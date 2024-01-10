4b9c3983774552e7a514d17b4e811a2334c2b634
marvinjoa <marvinjoa@me.com>
2024-01-10 16:46:56 +0100

    Update decrypt_preferences.js

--- tools/decrypt_preferences.js
+++ tools/decrypt_preferences.js
@@ -1,20 +1,20 @@
 'use strict';
 
-var crypto = require('crypto');
-var fs = require('fs');
+const crypto = require('crypto');
+const fs = require('fs');
 
-// extract the encoded, encrypted data
-var prefs = fs.readFileSync(process.argv[2] || 'LockSettingsPreferences.xml', 'utf8');
-var hexEncoded = /[0-9A-F]+(?=\<\/string\>)/.exec(prefs)[0];
-var cipherText = new Buffer(hexEncoded, 'hex');
+// Extract the encoded, encrypted data
+const prefs = fs.readFileSync(process.argv[2] || 'LockSettingsPreferences.xml', 'utf8');
+const hexEncoded = /[0-9A-F]+(?=\<\/string\>)/.exec(prefs)[0];
+const cipherText = Buffer.from(hexEncoded, 'hex');
 
-// decrypt
-var key = new Buffer('August#@3417r\0\0\0', 'utf8');
-var cipher = crypto.createDecipheriv('aes-128-ecb', key, '');
+// Decrypt
+const key = Buffer.from('August#@3417r\0\0\0', 'utf8');
+const cipher = crypto.createDecipheriv('aes-128-ecb', key, '');
 cipher.setAutoPadding(false);
-var plaintext = cipher.update(cipherText) + cipher.final();
+const plaintext = cipher.update(cipherText, 'hex', 'utf8') + cipher.final('utf8');
 
-// remove trailing nulls
-plaintext = plaintext.replace(/\0+$/, '');
+// Remove trailing nulls
+const trimmedPlaintext = plaintext.replace(/\0+$/, '');
 
-process.stdout.write(plaintext);
+process.stdout.write(trimmedPlaintext);
